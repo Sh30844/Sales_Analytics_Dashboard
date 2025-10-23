96598e97dc8374705adc58d1e5ef749f16bee4d2
Shidharth <shidharth906@gmail.com>
2025-10-23 18:46:04 +0530

    add_calculated_columns.sql

--- add_calculated_columns.sql
+++ add_calculated_columns.sql
@@ -8,4 +8,4 @@ SET `Total Amount` = (`Price Per Unit` * `Quantity Purchased`) - `Discount Appli
   
 ALTER TABLE Sales_Data ADD COLUMN `Profit` DECIMAL(10,2);
 UPDATE Sales_Data
-SET `Profit` = `Total Amount` - (`Cost Price` + `Quantity Purchased`);
+SET `Profit` = `Total Amount` - (`Cost Price` * `Quantity Purchased`);
